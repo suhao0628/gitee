2bb294c77074cff3ad4e70d92d7d50fed61fce9f
suhao <suhaoxb@gmail.com>
2024-05-24 00:24:10 +0700

    Add project files.

---
+++ gitee/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gitee.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
